9207c221f2dc3561b31ca1eed8752b57e9d81a4b
David Dukoski <david.dukoski@codechem.com>
2025-09-06 14:23:16 +0200

    :)

--- architecture/classification.py
+++ architecture/classification.py
@@ -4,6 +4,7 @@ from typing import override
 
 
 class NeuralEnsembleClassifier(NeuralEnsembleBase):
+
     def __init__(self, activation: ClassActivationName, **kwargs):
         super().__init__(**kwargs)
         self.activation_use = activation
